380ebde51db15a5acfb5c19e4735197083169107
jiun-dev <jiun.dev@gmail.com>
2023-03-25 18:38:16 +0900

    first commit

---
+++ src/users/users.controller.ts
@@ -0,0 +1,34 @@
+import { Body, Controller, Param, Post, Query, Get } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UserLoginDto } from './dto/user-login.dto';
+import { VerifyEmailDto } from './dto/verify-email.dto';
+import { UserInfo } from './UserInfo';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  async createUser(@Body() dto: CreateUserDto): Promise<void> {
+    console.log(dto);
+  }
+
+  @Post('/email-verify')
+  async verifyEmail(@Query() dto: VerifyEmailDto): Promise<string> {
+    console.log(dto);
+    return;
+  }
+
+  @Post('/login')
+  async login(@Body() dto: UserLoginDto): Promise<string> {
+    console.log(dto);
+    return;
+  }
+
+  @Get('/:id')
+  async getUserInfo(@Param('id') userId: string): Promise<UserInfo> {
+    console.log(userId);
+    return;
+  }
+}
